516ef0c2e173271b64fa8b91ced6f8564b337979
Mohammed Ahmed <raouf.ahmed248@gmail.com>
2021-12-13 19:02:29 -0800

    added build script for google benchmark install

---
+++ audioEffectsLib/benchmarkTests/buildScript.sh
@@ -0,0 +1,8 @@
+benchMarkDir="$1"
+cd "$benchMarkDir"
+git clone https://github.com/google/benchmark.git
+cd benchmark
+cmake -E make_directory "build"
+cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
+cmake --build "build" --config Release
+cmake -E chdir "build" ctest --build-config Release
