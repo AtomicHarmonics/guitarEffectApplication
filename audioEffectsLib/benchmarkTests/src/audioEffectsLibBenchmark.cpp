a6186578a9f6cc33aa9376f63e28e277fbe6f3c7
Mohammed Ahmed <raouf.ahmed248@gmail.com>
2021-12-13 17:59:25 -0800

    added tremolo effects to google benchmark

--- audioEffectsLib/benchmarkTests/src/audioEffectsLibBenchmark.cpp
+++ audioEffectsLib/benchmarkTests/src/audioEffectsLibBenchmark.cpp
@@ -10,8 +10,14 @@ BENCHMARK(BM_StringCreation);
 // Define another benchmark
 static void BM_StringCopy(benchmark::State& state) {
   audioEffects a;
+  float inputBuffer[512] = {};
+  float outputBuffer[512] = {};
+  for(int x = 0; x < 512; x++)
+  {
+    inputBuffer[x] = (float)x/512.0;
+  }
   for (auto _ : state)
-    a.returnFour();
+    a.tremoloEffect(inputBuffer, outputBuffer, 512);
 }
 BENCHMARK(BM_StringCopy);
 
