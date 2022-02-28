f1daea5df586469e63ddcd6671fe6da4dc1e20e4
e-manua1 <albert.zapata011@gmail.com>
2022-02-28 16:54:08 -0600

    updated bitcrusher algorithm

--- algoTesting_matlab/BitCrusher.m
+++ algoTesting_matlab/BitCrusher.m
@@ -1,38 +1,43 @@
-%%Distortion Algorithm
+%% Bitcrusher Algorithm
 clear; clc;
 
-[in, Fs] = audioread('bluesRiff_mono.wav');
+[in, Fs] = audioread('bluesRiff_mono_48000.wav');
 t = (0:length(in)-1)/Fs;
-t = t(:); %?
+t = t(:);     %?
+disp(Fs);
 
-%%for sinwave input: uncomment next 4 lines & comment out other 't'
+%% for sinwave input: uncomment next 4 lines & comment out other 't'
 % Fs = 44100; Ts = 1/Fs;
 % f = 3;
-% t = (0: Ts: 1).'; %  .' ??
+% t = (0: Ts: 1).';     %  .' ??
 % in = sin(2*pi*t*f); %basic sin wave
 
-% creating algorithm
-% nBits = 2;
+%% Resolution
+% nBits = 5; %6 seems to be a good value mixed with a downsampling of 8 (only downsampling value i have tested w/ this combo)
 % ampValues = 2 ^ (nBits - 1);
+% for n = 1:length(in)
+%     in(n,1) = ceil(ampValues * in(n,1)) * (1/ampValues); %
+% end
 
+%% Downsampling
 holdVal = in(1);
-downSample = 8;
+downSample = 8; %range 1-10 sounds ok, 1 is clean audio
 bitCounter = downSample;
 for n = 1:length(in)
-    %out(n,1) = ceil(ampValues * in(n,1)) * (1/ampValues);
-    if bitCounter == 0
+    if bitCounter <= 0
         holdVal = in(n);
         bitCounter = downSample;
-    end        
+    end
     out(n) = holdVal;
     bitCounter = bitCounter - 1;
 end
 
+%% Audio
 % sound(in * 0.1, Fs); pause(); % hit any key in the command window to unpause code exection
 sound(out * 0.1, Fs);
 % audiowrite('8bit.wav', out, Fs); %save output audiofile
 
-%Visuals
+%% Visuals
 figure;
 plot(t, in);
 
