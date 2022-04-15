a27feaded7abe169d33999a24fa007dfb0ecdbb7
Mohammed <raouf.ahmed248@gmail.com>
2022-04-14 23:02:14 -0500

    added updated LCD code

--- jackScripts/effectsDisplay.py
+++ jackScripts/effectsDisplay.py
@@ -21,6 +21,7 @@ from datetime import datetime
 import socket
 import json
 import requests
+from urllib.request import urlopen
 
 if os.name != 'posix':
     sys.exit('{} platform not supported'.format(os.name))
@@ -91,37 +92,54 @@ def retrieveConfig(device):
     font_path = str(Path(__file__).resolve().parent.joinpath('fonts', 'ProggyTiny.ttf'))
     font2 = ImageFont.truetype(font_path, 16)
 
-
+def internet_on():
+   try:
+        response = urlopen('https://www.google.com/', timeout=10)
+        return True
+   except: 
+        return False
+        
 #TODO: Add Overdrive parameters, add setting if effect is enabled or disabled, eventually add reverb configs when provided
-def updateDisplay(device, inputMessage):
+def updateDisplay(device, inputMessage,byPassStatus,internetVar):
     font_path = str(Path(__file__).resolve().parent.joinpath('fonts', 'ProggyTiny.ttf'))
-    font2 = ImageFont.truetype(font_path, 16)
+    font2 = ImageFont.truetype(font_path, 20)
+    #y = json.loads(inputMessage)
     y = inputMessage
+    z = byPassStatus
+    internetVar = internet_on()
     #add order number and enabled
     zeroLine= str(y["author"]) 
     firstLine = "TRM F:" + str(y["tremoloFreq"]) + " D:" + str(y["tremoloDepth"]) + " NUM: " + str(y["tremoloOrderNumber"]) + " EN:" + str(y["tremoloEnabled"])
     secondLine = "DST TRSH:" + str(y["distortThresh"]) + "  NUM: " + str(y["distortOrderNumber"]) + " EN:" + str(y["distortEnabled"]) 
     #create your lines here
-    thirdLine = "OVD TRSH:" + str(y["overDriveThresh"]) + "   NUM: "+str(y["overDriveOrderNumber"]) + " EN:" + str(y["overDriveEnabled"])
+    thirdLine = "OVD TRSH:" + str(y["overDriveThresh"]) + "  NUM: "+str(y["overDriveOrderNumber"]) + " EN:" + str(y["overDriveEnabled"])
     fourthLine = "PRE AMP: "+ str(y["preAmpGain"]) + "          EN:" +str(y["preAmpEnabled"])
     fifthLine = "RVRBDMPLVL:" + str(y["reverbDampLevel"]) + " NUM:" + str(y["reverbOrderNumber"])+ " EN:" + str(y["reverbEnabled"]) 
     sixthLine = "MOD: " + str(y["reverbMode"]) + " DRYLVL: " + str(y["reverbDryLevel"])+ " SIZE: " + str(y["reverbRoomSize"]) 
     seventhLine ="WETLVL: " + str(y["reverbWetLevel"])+ " WD: " + str(y["reverbWidth"])
     eighthLine = str(y["title"]) 
-   
+    bypassLine = "Bypass Status:" + str(z["bypassEnabled"])
+    internetLine = "Internet Connection Status:" + str(internetVar)
+    
+    
+    
    
     print(y["author"])
     with canvas(device) as draw:
-        draw.text((0, 0), firstLine, font=font2, fill="white")
+        draw.text((0, 0), zeroLine, font=font2, fill="white")
         if device.height >= 32:
-            draw.text((0, 8),  zeroLine, font=font2, fill="white")
-            draw.text((0, 16), secondLine, font=font2, fill="white")
-            draw.text((0, 24), thirdLine, font=font2, fill="white")
-            draw.text((0, 32), fourthLine, font=font2, fill="white")
-            draw.text((0, 40), fifthLine, font=font2, fill="white")
-            draw.text((0, 48), sixthLine, font=font2, fill="white")
-            draw.text((0, 56), seventhLine, font=font2, fill="white")
-            draw.text((0, 64), eighthLine, font=font2, fill="white")
+            draw.text((0, 20),  firstLine, font=font2, fill="white")
+            draw.text((0, 40), secondLine, font=font2, fill="white")
+            draw.text((0, 60), thirdLine, font=font2, fill="white")
+            draw.text((0, 80), fourthLine, font=font2, fill="white")
+            draw.text((0, 100), fifthLine, font=font2, fill="white")
+            draw.text((0, 120), sixthLine, font=font2, fill="white")
+            draw.text((0, 140), seventhLine, font=font2, fill="white")
+            draw.text((0, 160), eighthLine, font=font2, fill="white")
+            draw.text((0, 180), bypassLine, font=font2, fill="white")
+            draw.text((0, 200), internetLine, font=font2, fill="white")
+          
+            
            
            
             
@@ -137,16 +155,16 @@ def updateDisplay(device, inputMessage):
 
 def main():
 
-    
-    
-    while True:
-        x = requests.get('http://192.168.86.31:4996/effectsProfile/selectedProfile/')
-        print(x.status_code)
-        if(x.status_code == 200):
-            updateDisplay(device, x.json())
-        time.sleep(2)
+        while True:
+                x = requests.get('http://192.168.86.31:4996/effectsProfile/selectedProfile/')
+                z = requests.get('http://192.168.86.31:4996/byPass/')
+                internetVar = requests.get('https://www.google.com/')
+                print(x.status_code)
+                if(x.status_code == 200 and z.status_code == 200):
+                    updateDisplay(device, x.json(),z.json(),internetVar)
+                time.sleep(2)
        
-#python3 effectsDisplay.py --display pygame --width 240 --height 320
+#python3 effectsDisplay.py --display pygame --width 320 --height 240
 
 if __name__ == "__main__":
     try:
